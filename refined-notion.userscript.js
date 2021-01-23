f225b9182d272abf6a3d5f96ec79253b186cc961
Karl Horky <karl.horky@gmail.com>
2021-01-23 12:10:42 +0100

    Update refined-notion.userscript.js

--- refined-notion.userscript.js
+++ refined-notion.userscript.js
@@ -8,7 +8,7 @@
 // @description 1/23/2021, 11:45:43 AM
 // ==/UserScript==
 
-// Fix bug where Notion.so cmd-N shortcut overrides browser shortcut
+// Fix bug where Notion.so cmd-N shortcut overrides browser "New Window" shortcut
 // https://twitter.com/karlhorky/status/1352932541750247427
 document.addEventListener('keydown', (event) => {
   if (event.key === 'n' && event.metaKey === true) {
